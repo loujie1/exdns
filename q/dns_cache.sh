0fbc7f82dd7f932ec58798bfca5b8b11865b8993
Jie Lou <happyv1994@gmail.com>
2022-08-26 15:32:43 +0000

    test output for dnssec & dns

--- q/dns_cache.sh
+++ q/dns_cache.sh
@@ -21,23 +21,23 @@ queryL1(){
 }
 
 queryL2(){
-  ./q -port 53 -output="${1:-""}" -chain="3" @64.227.123.153 $l2
+  ./q -port 53 -output="${1:-""}" -chain="3" @157.230.27.159 $l2
 }
 
 queryL3(){
-  ./q -port 53 -output="${1:-""}" -chain="4" @@64.227.123.153 $l3
+  ./q -port 53 -output="${1:-""}" -chain="4" @157.230.27.159 $l3
 }
 
 queryL4(){
-  ./q -port 53 -output="${1:-""}" -chain="5" @@64.227.123.153 $l4
+  ./q -port 53 -output="${1:-""}" -chain="5" @157.230.27.159 $l4
 }
 
 queryL5(){
-  ./q -port 53 -output="${1:-""}" -chain="6" @@64.227.123.153 $l5
+  ./q -port 53 -output="${1:-""}" -chain="6" @157.230.27.159 $l5
 }
 
 queryL6(){
-  ./q -port 53 -output="${1:-""}" -chain="7" @@64.227.123.153 $l6
+  ./q -port 53 -output="${1:-""}" -chain="7" @157.230.27.159 $l6
 }
 
 createCache(){
@@ -50,11 +50,11 @@ createCache(){
 }
 
 queryAllCached(){
-  queryL2 ./len_3_cache_dns.csv &
-  queryL3 ./len_4_cache_dns.csv &
-  queryL4 ./len_5_cache_dns.csv &
-  queryL5 ./len_6_cache_dns.csv &
-  queryL6 ./len_7_cache_dns.csv &
+  queryL2 ./dns/len_3_cache_dns.csv &
+  queryL3 ./dns/len_4_cache_dns.csv &
+  queryL4 ./dns/len_5_cache_dns.csv &
+  queryL5 ./dns/len_6_cache_dns.csv &
+  queryL6 ./dns/len_7_cache_dns.csv &
   wait
 }
 #for qname in "a." "a.l1." "a.l2.l1.l2.l1." "a.l3.l2.l1.l3.l2.l1." "a.l4.l3.l2.l1." "a.l5.l4.l3.l2.l1." "a.l6.l5.l4.l3.l2.l1." "a.l7.l6.l5.l4.l3.l2.l1." "a.l8.l7.l6.l5.l4.l3.l2.l1.";do ./q -port 53 -rhine -cert=./testdata/certificate/CACert.pem @@64.227.123.153 $qname; done
@@ -67,19 +67,19 @@ queryWithCache(){
 }
 
 queryAllNoCache(){
-  queryL2 ./len_3_no_cache_dns.csv
-  sleep 3
-  queryL3 ./len_4_no_cache_dns.csv
-  sleep 3
-  queryL4 ./len_5_no_cache_dns.csv
-  sleep 3
-  queryL5 ./len_6_no_cache_dns.csv
-  sleep 3
-  queryL6 ./len_7_no_cache_dns.csv
-  sleep 3
+  queryL2 ./dns/len_3_no_cache_dns.csv
+  sleep 5
+  queryL3 ./dns/len_4_no_cache_dns.csv
+  sleep 5
+  queryL4 ./dns/len_5_no_cache_dns.csv
+  sleep 5
+  queryL5 ./dns/len_6_no_cache_dns.csv
+  sleep 5
+  queryL6 ./dns/len_7_no_cache_dns.csv
+  sleep 5
 }
 queryWithoutCache(){
   for run in {1..100}; do queryAllNoCache; done
 }
 
-queryWithCache
+queryWithoutCache
--- q/dnssec_cache.sh
+++ q/dnssec_cache.sh
@@ -11,23 +11,23 @@ l8="a.l8.l7.l6.l5.l4.l3.l2.l1."
 
 
 queryL2(){
-  ./q -port 53 -tcp -dnssec -check -output="${1:-""}" -chain="3" @64.227.123.153 $l2
+  ./q -port 53 -dnssec -check -output="${1:-""}" -chain="3" @64.227.123.153 $l2
 }
 
 queryL3(){
-  ./q -port 53 -tcp -dnssec -check -output="${1:-""}" -chain="4" @64.227.123.153 $l3
+  ./q -port 53 -dnssec -check -output="${1:-""}" -chain="4" @64.227.123.153 $l3
 }
 
 queryL4(){
-  ./q -port 53 -tcp -dnssec -check -output="${1:-""}" -chain="5" @64.227.123.153 $l4
+  ./q -port 53 -dnssec -check -output="${1:-""}" -chain="5" @64.227.123.153 $l4
 }
 
 queryL5(){
-  ./q -port 53 -tcp -dnssec -check -output="${1:-""}" -chain="6" @64.227.123.153 $l5
+  ./q -port 53 -dnssec -check -output="${1:-""}" -chain="6" @64.227.123.153 $l5
 }
 
 queryL6(){
-  ./q -port 53 -tcp -dnssec -check -output="${1:-""}" -chain="7" @64.227.123.153 $l6
+  ./q -port 53 -dnssec -check -output="${1:-""}" -chain="7" @64.227.123.153 $l6
 }
 
 createCache(){
@@ -56,20 +56,60 @@ queryWithCache(){
   for run in {1..100}; do queryAllCached; done
 }
 
+queryL2NoCache(){
+for run in {1..3}
+do
+  queryL2 ./len_3_no_cache_dnssec.csv
+  sleep 5
+done
+}
+
+queryL3NoCache(){
+for run in {1..3}
+do 
+  queryL3 ./len_4_no_cache_dnssec.csv
+  sleep 5
+done
+}
+
+queryL4NoCache(){
+for run in {1..3}
+do 
+  queryL4 ./len_5_no_cache_dnssec.csv
+  sleep 5
+done
+}
+
+queryL5NoCache(){
+for run in {1..3}
+do
+  queryL5 ./len_6_no_cache_dnssec.csv
+  sleep 5
+done
+}
+
+queryL6NoCache(){
+for run in {1..3}
+do
+  queryL6 ./len_7_no_cache_dnssec.csv
+  sleep 5
+done
+}
+
 queryAllNoCache(){
   queryL2 ./len_3_no_cache_dnssec.csv
-  sleep 3
+  sleep 5
   queryL3 ./len_4_no_cache_dnssec.csv
-  sleep 3
+  sleep 5
   queryL4 ./len_5_no_cache_dnssec.csv
-  sleep 3
+  sleep 5
   queryL5 ./len_6_no_cache_dnssec.csv
-  sleep 3
+  sleep 5
   queryL6 ./len_7_no_cache_dnssec.csv
-  sleep 3
+  sleep 5
 }
 queryWithoutCache(){
   for run in {1..100}; do queryAllNoCache; done
 }
 
-queryWithCache
+queryWithoutCache
